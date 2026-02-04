f36ddac5289ef704ff00d0c3904148a5f76bc6a7
Egen Endo <egenendo@gmail.com>
2026-02-04 21:22:08 +0700

    feat: initial commit project fastprint.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/test_programmer_egen_endo';
+$config['base_url'] = 'http://localhost/test-junior_programmer-egen';
 
 /*
 |--------------------------------------------------------------------------
